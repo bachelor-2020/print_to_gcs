d8772d5c4791cc64b5636c0882abee3528e0f409
Espen Trydal <espen@fastmail.com>
2020-04-20 17:12:18 +0200

    Leser melding fra GPS og objektgjenkjennings-nodene, synkroniserer meldingene og publiserer dem til skjermen

--- src/print_to_gcs.py
+++ src/print_to_gcs.py
@@ -1,48 +1,66 @@
 #! /usr/bin/env python
+# coding=utf-8
 from __future__ import print_function
-from vision_msgs.msg import Detection2DArray
-from sensor_msgs.msg import NavSatFix
+
 import rospy
-from rospy_message_converter import json_message_converter
-import json
 import message_filters
+
+from sensor_msgs.msg import NavSatFix
+from vision_msgs.msg import Detection2DArray
 
 
 class Print_to_gcs:
     def __init__(self):
         rospy.init_node("print_to_gcs")
 
-        sub_object_detection = rospy.Subscriber(
-            "/objects", Detection2DArray, self.print_object_to_gcs
+        sub_object_detection = message_filters.Subscriber(
+            "/objects", Detection2DArray, queue_size=1
         )
-        sub_gps = rospy.Subscriber(
-            "/mavros/global_position/global", NavSatFix, self.print_gps_to_gcs
+
+        # sub_gps = message_filters.Subscriber("/mavros/global_position/global", NavSatFix, queue_size=10) # Fra Ardupilot
+        sub_gps = message_filters.Subscriber(
+            "/gps/fix", NavSatFix, queue_size=1
+        )  # FakeGPS
+
+        msg_sync = message_filters.ApproximateTimeSynchronizer(
+            [sub_object_detection, sub_gps], 10, 0.1
         )
+        msg_sync.registerCallback(self.print_to_gcs)
 
         self.rate = rospy.Rate(2)
 
-    def print_object_to_gcs(self, msg):
-        # msg_dict = message_converter.convert_ros_message_to_dictionary(msg)
-        # id = msg_dict["detections"][1]["results"][0]["id"]
-        # msg_json = json_message_converter.convert_ros_message_to_json(msg)
-        # print(json.dumps(msg_json, sort_keys=True, indent=4, separators=(",", ": ")))
+    def print_to_gcs(self, msg_obj, msg_gps):
+        print("\nGPS data:")
+        print("\tTidsstempel:")
+        print("\t\tsekunder:", msg_gps.header.stamp.secs)
+        print("\t\tnanosekunder:", msg_gps.header.stamp.nsecs)
+        print(
+            "\tbreddegrad:", msg_gps.latitude
+        )  # Positivt tall er nord for ekvator, negativt er sør
+        print(
+            "\tlengdegrad:", msg_gps.longitude
+        )  # Positivt tall er øst for Greenwich, negativt vest
+        print(
+            "\thøyde:".decode("utf-8"), msg_gps.altitude
+        )  #  NaN hvis ingen høyde er tilgjengelig
+        print("\tstatus gps:", msg_gps.status.status, end="")
+        if msg_gps.status.status >= 0:
+            print(" -- OK")
+        else:
+            print(" -- ikke fått gps fix".decode("utf-8"))
 
-        if len(msg.detections) > 0:
-            print("Antall objekter funnet: ", len(msg.detections))
-            for i in range(len(msg.detections)):
-                print("Objekt: ", i, ": ")
+        if len(msg_obj.detections) > 0:
+            print("\nAntall objekter funnet: ", len(msg_obj.detections))
+            for i in range(len(msg_obj.detections)):
+                print("\tObjekt: ", i, ": ")
                 # for j in range(len(msg.detections[i].results)): # I test er len alltid 1
-                print("id: ", msg.detections[i].results[0].id)
+                print("\t\tid: ", msg_obj.detections[i].results[0].id)
 
-                if msg.detections[i].results[0].id == 1:
-                    print("Person funnet!")
+                if msg_obj.detections[i].results[0].id == 1:
+                    print("\t\t** Person funnet! **")
 
         else:
-            print("Ingen objekter funnet")
-
-    def print_gps_to_gcs(self, msg):
-        print(msg.latitude)
-        print(msg.longitude)
+            print("\nIngen objekter funnet")
 
 
 if __name__ == "__main__":
