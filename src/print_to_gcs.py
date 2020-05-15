787b43c40154f6a5fd5f144d8ce7b74ebd34e389
Espen Trydal <espen@fastmail.com>
2020-05-15 11:32:23 +0200

    Getting image from darknet when object found. Display not working.

--- src/print_to_gcs.py
+++ src/print_to_gcs.py
@@ -2,68 +2,82 @@
 # coding=utf-8
 from __future__ import print_function
 
-from PIL import Image
+import PIL.Image
 import numpy as np
+from StringIO import StringIO
 
 import rospy
 import message_filters
 
 from sensor_msgs.msg import NavSatFix
-from vision_msgs.msg import Detection2DArray
+from sensor_msgs.msg import Image
+from darknet_ros_msgs.msg import BoundingBoxes
 
 
 class Print_to_gcs:
     def __init__(self):
         rospy.init_node("print_to_gcs")
 
-        sub_object_detection = message_filters.Subscriber(
-            "objects", Detection2DArray, queue_size=1
+        sub_detection_image = message_filters.Subscriber(
+            "darknet_ros/detection_image", Image, queue_size=1
         )
 
-        sub_gps = message_filters.Subscriber(
-            "mavros/global_position/global", NavSatFix, queue_size=1
-        )  # Fra Ardupilot
+        sub_detection_box = message_filters.Subscriber(
+            "darknet_ros/bounding_boxes", BoundingBoxes, queue_size=1
+        )
 
-        # sub_gps = message_filters.Subscriber("gps/fix", NavSatFix, queue_size=1)  # FakeGPS
+        # sub_gps = message_filters.Subscriber(
+        #     "mavros/global_position/global", NavSatFix, queue_size=1
+        # )  # Fra Ardupilot
+
+        sub_gps = message_filters.Subscriber(
+            "gps/fix", NavSatFix, queue_size=1
+        )  # FakeGPS
 
         msg_sync = message_filters.ApproximateTimeSynchronizer(
-            [sub_object_detection, sub_gps], 10, 0.1
+            [sub_detection_image, sub_detection_box, sub_gps], 10, 0.1
         )
         msg_sync.registerCallback(self.print_to_gcs)
 
         self.rate = rospy.Rate(2)
 
-    def print_to_gcs(self, msg_obj, msg_gps):
+    def print_to_gcs(self, msg_image, msg_box, msg_gps):
         print("\nGPS data:")
         print("\tTidsstempel:")
-        print("\t\tsekunder:", msg_gps.header.stamp.secs)
-        print("\t\tnanosekunder:", msg_gps.header.stamp.nsecs)
+        print("\t\tSekunder:", msg_gps.header.stamp.secs)
+        print("\t\tNanosekunder:", msg_gps.header.stamp.nsecs)
         print(
-            "\tbreddegrad:", msg_gps.latitude
+            "\tBreddegrad:", msg_gps.latitude
         )  # Positivt tall er nord for ekvator, negativt er sør
         print(
-            "\tlengdegrad:", msg_gps.longitude
+            "\tLengdegrad:", msg_gps.longitude
         )  # Positivt tall er øst for Greenwich, negativt vest
         print(
-            "\thøyde:".decode("utf-8"), msg_gps.altitude
+            "\tHøyde:".decode("utf-8"), msg_gps.altitude
         )  #  NaN hvis ingen høyde er tilgjengelig
-        print("\tstatus gps:", msg_gps.status.status, end="")
+        print("\tStatus gps:", msg_gps.status.status, end="")
         if msg_gps.status.status >= 0:
             print(" -- OK")
         else:
-            print(" -- ikke fått gps fix".decode("utf-8"))
+            print(" -- Ikke fått gps fix".decode("utf-8"))
 
-        if len(msg_obj.detections) > 0:
-            print("\nAntall objekter funnet: ", len(msg_obj.detections))
-            for i in range(len(msg_obj.detections)):
+        if len(msg_box.bounding_boxes) > 0:
+            print("\nAntall objekter funnet: ", len(msg_box.bounding_boxes))
+            for i in range(len(msg_box.bounding_boxes)):
                 print("\tObjekt: ", i, ": ")
-                # for j in range(len(msg.detections[i].results)): # I test er len alltid 1
-                print("\t\tid: ", msg_obj.detections[i].results[0].id)
+                print("\tTid: ", msg_box.bounding_boxes[i].id)
 
-                if msg_obj.detections[i].results[0].id == 1:
+                if msg_box.bounding_boxes[i].id == 0:
                     print("\t\t** Person funnet! **")
-                    data = msg_obj.detections[i].source_img.data
-                    print(type(data))
+                    # data = bytes(msg_image.data)
+                    # data_array = np.frombuffer(msg_image.data, dtype=np.uint8)
+
+                    img = PIL.Image.frombytes(
+                        "RGB", (msg_image.height, msg_image.width), msg_image.data
+                    )
+                    # img = PIL.Image.fromarray("RGB", data_array)
+                    print(type(img))
+                    img.save("test-[i].jpg")
 
         else:
             print("\nIngen objekter funnet")
